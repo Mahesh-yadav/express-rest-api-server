910a625903f97a6ff3717d222092211417f6cba5
Mahesh Yadav <mahesh.iitmandi@gmail.com>
2020-07-22 09:49:30 +0530

    generate fake data

---
+++ src/utils/generateFakeData.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import path from 'path';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import faker from 'faker';
+import cuid from 'cuid';
+
+const FILE_URI = path.join(__dirname, '../', 'products.json');
+
+const products = [];
+
+for (let i = 0; i < 5; i++) {
+  const id = cuid();
+  const name = faker.commerce.product();
+  const price = faker.commerce.price();
+
+  products.push({
+    id,
+    name,
+    price,
+  });
+}
+
+fs.writeFileSync(FILE_URI, JSON.stringify(products));
+
+console.log('Data generated.');
